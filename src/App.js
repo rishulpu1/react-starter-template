d35330498140d13a50ef0db4b66cb12a358c05f7
rajneeshibs <48013416+rajneeshibs@users.noreply.github.com>
2023-09-29 13:54:30 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import NotFound from './pages/404/NotFound'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h2>Home page</h2>
-    </div>
+    <>
+        <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        <Footer />
+    </>
   )
 }
 
